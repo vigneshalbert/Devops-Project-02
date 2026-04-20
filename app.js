73d4a22ee9e69cb361fe285aa4e67b7dc326551a
Vignesh A <vignesh.albert@MacBook-Pro.local>
2026-04-20 10:29:25 +0530

    ci/cd test changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("🚀 DevOps Project 2 Running Successfully!");
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Jenkins CI/CD Working 🚀 Version 2");;
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on port 3000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
